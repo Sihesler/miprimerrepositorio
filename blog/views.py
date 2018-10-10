2469d2f1d9dcc593177a5b34376bfd22bd2fdc1d
Sihesler <siheslermateo@gmail.com>
2018-10-09 23:16:09 -0600

    Agragar y editar publicaciones

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
-
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def listado(request):
     posts = Publicacion.objects.filter(fecha_creacion__lte = timezone.now()).order_by('fecha_creacion')
@@ -10,3 +11,31 @@ def listado(request):
 def detalle(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/detalle.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_creacion = timezone.now()
+            post.save()
+            return redirect('detalle', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('detalle', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
