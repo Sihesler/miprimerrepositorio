f8925813288117d2d15dd0176f6cc647b4c3a2b0
Sihesler <siheslermateo@gmail.com>
2018-11-03 13:26:49 -0600

    Login

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Publicacion
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def listado(request):
     posts = Publicacion.objects.filter(fecha_creacion__lte = timezone.now()).order_by('fecha_creacion')
@@ -13,6 +14,7 @@ def detalle(request, pk):
     return render(request, 'blog/detalle.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -27,6 +29,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     if request.method == "POST":
@@ -42,6 +45,7 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
     posts = Publicacion.objects.filter(fecha_creacion__isnull=True).order_by('fecha_creacion')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
@@ -52,7 +56,7 @@ def post_publish(request, pk):
     post.publicar()
     return redirect('detalle', pk=pk)
 
-
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     post.eliminar()
